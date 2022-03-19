762d770c4749539994b2c992c60b2e206aa28b39
Sangmin <23mean@naver.com>
2022-03-19 17:23:24 +0900

    fix: 변수명 수정

--- Sangmin/2/app.js
+++ Sangmin/2/app.js
@@ -1,32 +1,31 @@
-const container = document.querySelector(".container");
-const seat = document.querySelectorAll(".row .seat");
-const count = document.querySelector("#count");
-const total = document.querySelector("#total");
+const movieContainer = document.querySelector(".container");
+const clickCount = document.querySelector("#count");
+const totalPrice = document.querySelector("#total");
 
-let counting = 0;
+let count = 0;
 let price = 10;
 
 const seatClick = (e) => {
   if (e.target.className === "seat") {
     e.target.classList.add("selected");
-    counting++;
-    count.innerHTML = counting;
-    total.innerHTML = counting * price;
+    showValue(++count, price);
     return;
   }
   if (e.target.className === "seat selected") {
     e.target.classList.remove("selected");
-    counting--;
-    count.innerHTML = counting;
-    total.innerHTML = counting * price;
+    showValue(--count, price);
     return;
   }
 };
 
-const handleOnChange = (e) => {
-  price = Number(e.value);
-  count.innerHTML = counting;
-  total.innerHTML = counting * price;
+const showValue = (count, price) => {
+  clickCount.innerHTML = count;
+  totalPrice.innerHTML = count * price;
 };
 
-container.addEventListener("click", seatClick);
+const ChangeMovie = (e) => {
+  price = Number(e.value);
+  showValue(count, price);
+};
+
+movieContainer.addEventListener("click", seatClick);
