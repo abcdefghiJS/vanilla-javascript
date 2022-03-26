b46d1485a84355c65fbaa696ed674a6201aa3106
leehyewon0531 <ifpffk@naver.com>
2022-03-26 23:48:20 +0900

    fix: PR 리뷰 반영

--- hyewon/2-movieSeatBooking/script.js
+++ hyewon/2-movieSeatBooking/script.js
@@ -2,12 +2,12 @@ const $container = document.querySelector('.container')
 const $seats = document.querySelectorAll('.row .seat')
 let $select = document.getElementById('movie')
 let $selectIndex = document.getElementById('movie').options.selectedIndex
-let v = $select.options[$selectIndex].value
+let movieValue = $select.options[$selectIndex].value
 const $count = document.getElementById('count')
 const $total = document.getElementById('total')
 
 if(localStorage.getItem('value') === null) {
-    localStorage.setItem('value', v)
+    localStorage.setItem('value', movieValue)
 }
 populateUI()
 
@@ -15,23 +15,29 @@ populateUI()
 function showSelected(seat) {
     if(seat.className === "seat") {
         seat.className = "seat selected"
-    } else if (seat.className === "seat selected") {
+        return
+    }
+    if (seat.className === "seat selected") {
         seat.className = "seat"
+        return
     }
 }
 
 // 선택된 좌석 수와 총 가격을 보여주는 함수
 function stateChange(seat) {
     if(seat.className === "seat selected"){
-        $count.innerText = parseInt($count.innerText) + 1
-        $total.innerText = parseInt($total.innerText) + parseInt(v)
-        localStorage.setItem('count', $count.innerText)
-        localStorage.setItem('total', $total.innerText)
-    } else if(seat.className === "seat") {
-        $count.innerText = parseInt($count.innerText) - 1
-        $total.innerText = parseInt($total.innerText) - parseInt(v)
-        localStorage.setItem('count', $count.innerText)
-        localStorage.setItem('total', $total.innerText)
+        $count.textContent = parseInt($count.textContent) + 1
+        $total.textContent = parseInt($total.textContent) + parseInt(movieValue)
+        localStorage.setItem('count', $count.textContent)
+        localStorage.setItem('total', $total.textContent)
+        return
+    } 
+    if(seat.className === "seat") {
+        $count.textContent = parseInt($count.textContent) - 1
+        $total.textContent = parseInt($total.textContent) - parseInt(movieValue)
+        localStorage.setItem('count', $count.textContent)
+        localStorage.setItem('total', $total.textContent)
+        return
     }
 }
 
@@ -40,15 +46,15 @@ function movieSelect() {
     $select = document.getElementById('movie')
     $selectIndex = document.getElementById('movie').options.selectedIndex
 
-    v = $select.options[$selectIndex].value // 가격을 계산하기 위해 value 값 저장
+    movieValue = $select.options[$selectIndex].value // 가격을 계산하기 위해 value 값 저장
 
     // 영화 종류에 따라 총 가격이 다르게 계산되어야 함
-    if($count.innerText !== "0") {
-        $total.innerText = parseInt(v) * parseInt($count.innerText)
-        localStorage.setItem('total', $total.innerText)
+    if($count.textContent !== "0") {
+        $total.textContent = parseInt(movieValue) * parseInt($count.textContent)
+        localStorage.setItem('total', $total.textContent)
     }
 
-    localStorage.setItem('value', v)
+    localStorage.setItem('value', movieValue)
 }
 
 // 선택된 좌석 상태를 local storage 에 업데이트 해주는 함수
@@ -68,8 +74,8 @@ function populateUI() {
                 seat.className = "seat selected"
             }
         })
-        $count.innerText = localStorage.getItem('count')
-        $total.innerText = localStorage.getItem('total')
+        $count.textContent = localStorage.getItem('count')
+        $total.textContent = localStorage.getItem('total')
     }
     $select.value = localStorage.getItem('value')
     movieSelect()
@@ -79,5 +85,4 @@ $container.addEventListener('click', e => {
     showSelected(e.target)
     stateChange(e.target)
     update()
-    console.log(window.localStorage)
 })
